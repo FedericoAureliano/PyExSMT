3afb2e846c292e701f8610785c078314243ba239
FedericoAureliano <fmorarocha@gmail.com>
2017-12-16 10:23:25 -0500

    Updated graph gen

--- symbolic/result.py
+++ symbolic/result.py
@@ -31,56 +31,44 @@ class Result(object):
         self.execution_return_values.append(ret)
 
     def to_dot(self, filename):
-        print("HEY")
         graph = self.get_graph_dict()
-        print("HERE", graph)
-        dot = self.graph_to_dot(graph)
-        print("YO", dot)
+        dot = self.dict_to_dot(graph)
         s = Source(dot, filename=filename+".dot", format="png")
         s.view()
 
     def get_graph_dict(self):
         graph = {} #nodes are keys, edges are elements of the list items
         queue = deque([self.path.root_constraint])
-        print("0")
         while len(queue) > 0:
-            print("0.5")
             c = queue.popleft()
+            queue.extend(c.children)
             if c.predicate is None:
                 continue
-            queue.extend(c.children)
             rep = c.predicate.symtype.__repr__()
-            print("1")
             if not rep in graph:
-                print("2")
                 #initialize node with no edges out
-                graph[rep] = []
-            if c.parent is not None:
-                print("3")
+                graph[rep] = [None, None]
+            if c.parent is not None and c.parent.predicate is not None:
                 prep = c.parent.predicate.symtype.__repr__()
-                if c.parent.predicate.result:
-                    # if we are the true branch,
-                    # add an edge from the parent to this node on the left
-                    graph[prep] = [rep] + graph[prep]
-                else:
-                    graph[prep] = graph[prep]+ [rep]
-                if len(graph[prep]) > 2:
-                    raise ValueError("%s IS NOT BINARY!!" % prep)
+                slot = 1 if c.parent.predicate.result else 0
+                graph[prep][slot] = rep
         return graph
 
-    def graph_to_dot(self, graph):
-        # print the thing into DOT format
+    def dict_to_dot(self, graph):
         header = "digraph {\n"
         footer = "\n}\n"
         dot = ""
         for node, edges in graph.items():
-            dot += node + " [ label=\"" + node + "\" ];\n"
-            for edge in edges:
-                dot += node + " -> " + edge + ";\n"
+            dot += "\"" + node + "\" [ label=\"" + node + "\" ];\n"
+            for result in range(2):
+                edge = edges[result]
+                if edge is None:
+                    continue
+                dot += "\"" + node + "\"  -> " + "\"" + edge + "\" [ label=\"" + str(result) + "\" ];\n"
         return header + dot + footer
 
     def to_summary(self):
         print("\nSUMMARY:")
-        for p,e in self.summary:
+        for p, e in self.summary:
             print("PATH: %s" % p)
             print("EFFECT: %s\n" % e.__repr__())
