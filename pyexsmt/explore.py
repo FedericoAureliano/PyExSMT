25cf8de3dad66d9132f64ad87f471e2d41356193
FedericoAureliano <fmorarocha@gmail.com>
2018-04-11 17:39:35 -0400

    most naive implementation of shadow symbolic executio for freud. Not even really shadow symbolic execution at this point.

--- pyexsmt/explore.py
+++ pyexsmt/explore.py
@@ -6,90 +6,61 @@ import logging
 from pyexsmt.path_to_constraint import PathToConstraint
 from pyexsmt import pred_to_smt
 from pyexsmt.symbolic_types import symbolic_object
-from pyexsmt.result import Result
+from pyexsmt.symbolic_types.symbolic_object import to_pysmt
 
 from pysmt.shortcuts import *
 
 
 class ExplorationEngine:
-    def __init__(self, funcinv, solver="z3"):
-        self.invocation = funcinv
-        # the input to the function
+    def __init__(self, funcinv, solver, max_iterations=0, max_depth=0, mod=None):
+        self.invocation      = funcinv
+        self.max_iterations  = max_iterations
         self.symbolic_inputs = {}  # string -> SymbolicObject
         # initialize
         for n in funcinv.get_names():
             self.symbolic_inputs[n] = funcinv.create_arg_value(n)
-
-        self.constraints_to_solve = deque([])
         self.num_processed_constraints = 0
-
-        self.path = PathToConstraint(lambda c : self.add_constraint(c))
+        self.path = PathToConstraint(solver, max_depth, mod)
         # link up SymbolicObject to PathToConstraint in order to intercept control-flow
         symbolic_object.SymbolicObject.SI = self.path
-
-        self.solver = Solver(solver)
-        self.solver.solve() # generate initial values
         # link up SymbolicObject to the Solver to get concrete values during execution
-        symbolic_object.SymbolicObject.SOLVER = self.solver 
+        symbolic_object.SymbolicObject.SOLVER = self.path.solver
 
-        # outputs
-        self.result = Result(self.path)
-
-    def add_constraint(self, constraint):
-        logging.debug("ADDING CONSTRAINT: %s", repr(constraint))
-        self.constraints_to_solve.append(constraint)
-
-    def explore(self, max_iterations=0, max_depth=0, funcs=[], mod=None):
-        self.path.max_depth = max_depth
-        self.path.mod = mod
-
+    def explore(self, funcs=[]):
         self._one_execution(funcs)
         
         iterations = 1
-        if max_iterations != 0 and iterations >= max_iterations:
+        if self.max_iterations != 0 and iterations >= self.max_iterations:
             logging.debug("Maximum number of iterations reached, terminating")
-            return self.result
+            return self.path
 
-        while not self._is_exploration_complete():
-            selected = self.constraints_to_solve.popleft()
+        while not self.path.is_exploration_complete():
+            selected = self.path.get_next_constraint()
             if selected.processed:
                 continue		
 
-            logging.debug("SELECTED CONSTRAINT: %s", repr(selected))
-            asserts, query = selected.get_asserts_and_query()
-            self._find_counterexample(asserts, query)
+            logging.debug("SELECTED CONSTRAINT: %s", selected)
+            model = self.path.solve_constraint(selected)
 
-            if not self.solver.last_result:
+            if not model:
                 continue
 
             self._one_execution(funcs, selected)
 
-            iterations += 1			
-            self.num_processed_constraints += 1
+            iterations += 1
 
-            if max_iterations != 0 and iterations >= max_iterations:
+            if self.max_iterations != 0 and iterations >= self.max_iterations:
                 logging.debug("Maximum number of iterations reached, terminating")
                 break
 
-        return self.result
-
-    # private
-
-    def _is_exploration_complete(self):
-        num_constr = len(self.constraints_to_solve)
-        if num_constr == 0:
-            logging.info("EXPLORATION COMPLETE")
-            return True
-        else:
-            logging.debug("%d constraints yet to solve (total: %d, already solved: %d)", \
-            num_constr, self.num_processed_constraints + num_constr, self.num_processed_constraints)
-            return False
+        return self.path
 
     def _one_execution(self, funcs=[], expected_path=None):
+        self.path.shadowing = False
+        symbolic_object.SymbolicObject.SI = self.path
+        self.path.record_inputs(self.symbolic_inputs)
+        logging.info("USING INPUTS: %s", self.path.generated_inputs[-1])
         logging.debug("EXPECTED PATH: %s", expected_path)
-
-        self.result.record_inputs(self.symbolic_inputs)
-        logging.info("USING INPUTS: %s", self.result.generated_inputs[-1])
 
         self.path.reset(expected_path)
 
@@ -98,12 +69,87 @@ class ExplorationEngine:
         except Exception:
             ret = None
 
-        logging.debug("CURRENT CONSTARINT: %s", repr(self.path.current_constraint))
+        logging.debug("CURRENT CONSTARINT: %s", self.path.current_constraint)
         logging.info("RETURN: %s", ret)
 
-        self.result.record_output(ret)
+        self.path.record_output(ret)
+        return ret
 
-    def _find_counterexample(self, asserts, query):
-        assumptions = [pred_to_smt(p) for p in asserts] + [Not(pred_to_smt(query))]
-        logging.debug("SOLVING: %s", assumptions)
-        self.solver.solve(assumptions)
+    def _one_shadow_execution(self, funcs=[]):
+        self.path.shadowing = True
+        symbolic_object.SymbolicObject.SI = self.path
+        try:
+            ret = self.invocation.call_function(self.symbolic_inputs, funcs)
+        except Exception:
+            ret = None
+        return ret
+
+class ShadowExplorationEngine:
+    def __init__(self, funcinv, shadowinv, solver, max_iterations=0, max_depth=0, mod=None):
+        self.solver = solver
+        self.engines = []
+        self.engines.append(ExplorationEngine(funcinv, solver, max_iterations, max_depth, mod))
+        self.engines.append(ExplorationEngine(shadowinv, solver, max_iterations, max_depth, mod))
+        self.lead = 0
+        self.max_iterations = max_iterations
+
+    def explore(self, funcs=[]):
+        original = self.engines[0]
+        shadow = self.engines[1]
+        r1, r2 = self._one_execution(funcs)
+        if not self.solver.get_py_value(Equals(to_pysmt(r1), to_pysmt(r2))):
+            logging.info("COUNTER! %s != %s", repr(r1), repr(r2))
+            return self.engines[self.lead-1].path.generated_inputs[-1]
+
+        iterations = 1
+        if self.max_iterations != 0 and iterations >= self.max_iterations:
+            logging.debug("Maximum number of iterations reached, terminating")
+            return (original.path, shadow.path)
+
+        while not self.is_exploration_complete():
+            selected = self.get_next_constraints()
+
+            logging.debug("SELECTED CONSTRAINT: %s", selected)
+            model = self.engines[self.lead].path.solve_constraint(selected)
+
+            if not model:
+                continue
+
+            #TODO avoid executing things unnecesarily? 
+            r1, r2 = self._one_execution(funcs, selected)
+
+            if not self.solver.get_py_value(Equals(to_pysmt(r1), to_pysmt(r2))):
+                logging.info("COUNTER! %s != %s", repr(r1), repr(r2))
+                return self.engines[self.lead-1].path.generated_inputs[-1]
+
+            iterations += 1
+
+            if self.max_iterations != 0 and iterations >= self.max_iterations:
+                logging.debug("Maximum number of iterations reached, terminating")
+                break
+
+        return (original.path, shadow.path)
+
+    def get_next_constraints(self):
+        lead = self.engines[self.lead]
+        follow = self.engines[self.lead-1]
+        if not lead.path.is_exploration_complete():
+            return lead.path.get_next_constraint()
+        elif not follow.path.is_exploration_complete():
+            self.lead = (self.lead + 1) % 2
+            return follow.path.get_next_constraint()
+        else:
+            raise ValueError("No Constraints left!")
+
+    def is_exploration_complete(self):
+        return all([x.path.is_exploration_complete() for x in self.engines])
+
+    def _one_execution(self, funcs, selected=None):
+        if selected is None:
+            r1 = self.engines[self.lead]._one_execution(funcs)
+            r2 = self.engines[self.lead-1]._one_execution(funcs)
+        else:
+            r1 = self.engines[self.lead]._one_execution(funcs, selected)
+            r2 = self.engines[self.lead-1]._one_shadow_execution(funcs)
+        self.lead = (self.lead + 1) % 2
+        return r1, r2
--- pyexsmt/path_to_constraint.py
+++ pyexsmt/path_to_constraint.py
@@ -1,22 +1,40 @@
 # Copyright: see copyright.txt
 
 import logging
+from graphviz import Source
 
-from pyexsmt.predicate import Predicate
+from collections import deque
+
+from pyexsmt.symbolic_types.symbolic_object import to_pysmt, is_instance_userdefined_and_newclass
+from pyexsmt import pred_to_smt, get_concr_value, match_smt_type
+from pyexsmt.symbolic_types import SymbolicObject
 from pyexsmt.constraint import Constraint
+from pyexsmt.predicate import Predicate
 
-from pyexsmt import pred_to_smt
 from pysmt.shortcuts import *
 
 class PathToConstraint:
-    def __init__(self, add):
-        self.constraints = {}
-        self.add = add
+    def __init__(self, solver, max_depth, mod):
+        self.solver = solver
+        self.max_depth = max_depth
+        if mod is not None:
+            self.mod = mod
+        else:
+            self.mod = TRUE()
+        # generate initial values
+        self.solver.solve([self.mod])  
+
         self.root_constraint = Constraint(None, None)
         self.current_constraint = self.root_constraint
         self.expected_path = None
-        self.max_depth = 0
-        self.mod = None
+        self.constraints_to_solve = deque([])
+
+        self.generated_inputs = []
+        self.execution_return_values = []
+        self.list_rep = None
+        self.curr_id = 0
+
+        self.shadowing = False
 
     def reset(self,expected):
         self.current_constraint = self.root_constraint
@@ -37,6 +55,9 @@ class PathToConstraint:
             logging.debug("Max Depth (%d) Reached", self.max_depth)
             return
 
+        if self.shadowing:
+            return
+
         # add both possible predicate outcomes to constraint (tree)
         p = Predicate(symbolic_type, branch)
         p.negate()
@@ -49,11 +70,12 @@ class PathToConstraint:
             if self.mod is not None and not is_sat(And(self.mod, pred_to_smt(p), *asserts)):
                 logging.debug("Path pruned by mod (%s): %s %s", self.mod, c, p)
                 return
+
             c = self.current_constraint.add_child(p)
 
             # we add the new constraint to the queue of the engine for later processing
             logging.debug("New constraint: %s", c)
-            self.add(c)
+            self.add_constraint(c)
             
         # check for path mismatch
         # IMPORTANT: note that we don't actually check the predicate is the
@@ -66,14 +88,119 @@ class PathToConstraint:
             logging.debug("DONE: %s; EXP: %s; C: %s", done, expected, c)
             if ( not done and expected.result != c.predicate.result or \
                 done and expected.result == c.predicate.result ):
-                print("Replay mismatch (done=",done,")")
-                print(expected)
-                print(c.predicate)
+                logging.info("Replay mismatch (done=%s)",done)
+                logging.info(expected)
+                logging.info(c.predicate)
 
         if cneg is not None:
             # We've already processed both
             cneg.processed = True
             c.processed = True
-            logging.debug("Processed constraint: %s", c)
+            logging.info("Processed constraint: %s", c)
 
         self.current_constraint = c
+
+    def add_constraint(self, c):
+        self.constraints_to_solve.append(c)
+
+    def get_next_constraint(self):
+        return self.constraints_to_solve.popleft()
+
+    def is_exploration_complete(self):
+        return len(self.constraints_to_solve) == 0
+
+    def solve_constraint(self, selected):
+        #TODO Check if we already have a solution to it first
+        #TODO Do local search if we have an almost solution?
+        asserts, query = selected.get_asserts_and_query()
+        assumptions = [self.mod] + [pred_to_smt(p) for p in asserts] + [Not(pred_to_smt(query))]
+        self.solver.solve(assumptions)
+        logging.debug("SOLVING: %s", assumptions)
+        return self.solver.last_result
+
+    def record_inputs(self, inputs):
+        inputs = [(k, get_concr_value(inputs[k])) for k in inputs]
+        self.generated_inputs.append(inputs)
+        logging.debug("RECORDING INPUTS: %s", inputs)
+
+    def record_output(self, ret):
+        logging.info("RECORDING EFFECT: %s -> %s", self.current_constraint, repr(ret))
+
+        self.current_constraint.effect = ret
+
+        if isinstance(ret, SymbolicObject):
+            ret = ret.get_concr_value()
+        self.execution_return_values.append(ret)
+
+    def to_dot(self, filename):
+        header = "digraph {\n"
+        footer = "}\n"
+        if self.list_rep is None:
+            self.list_rep = self._to_list_rep(self.root_constraint)
+        dot = self._to_dot(self.list_rep)
+        dot = header + dot + footer
+        s = Source(dot, filename=filename+".dot", format="png")
+        s.view()
+
+    def _to_dot(self, list_rep):
+        curr = self.curr_id
+        if isinstance(list_rep, list) and len(list_rep) == 3:
+            rep = list_rep[0]
+            dot = "\"%s%d\" [ label=\"%s\" ];\n" % (rep, curr, rep)
+            self.curr_id += 1
+
+            for slot in range(1, 3):
+                child = list_rep[slot]
+                if child is None:
+                    continue
+                crep = child[0] if isinstance(child, list) else to_pysmt(child)
+                crep = str(crep).replace('"', '\\\"')
+                dot += "\"%s%d\" -> \"%s%d\" [ label=\"%d\" ];\n" \
+                        %(rep, curr, crep, self.curr_id, slot%2)
+                dot += self._to_dot(child)
+            return dot
+        elif list_rep is not None:
+            list_rep = to_pysmt(list_rep)
+            list_rep = str(list_rep).replace('"', '\\\"')
+            temp = "\"%s%d\" [ label=\"%s\" ];\n" % (list_rep, curr, list_rep)
+            self.curr_id += 1
+            return temp
+        else:
+            return ""
+
+    def to_summary(self, unknown=Symbol('Unknown', INT)):
+        if self.list_rep is None:
+            self.list_rep = self._to_list_rep(self.root_constraint)
+        summary = self._to_summary(self.list_rep, unknown)
+        return summary
+
+    def _to_summary(self, list_rep, unknown):
+        if isinstance(list_rep, list) and len(list_rep) == 3:
+            return Ite(list_rep[0], self._to_summary(list_rep[1], unknown),\
+                        self._to_summary(list_rep[2], unknown))
+        elif list_rep is not None:
+            if isinstance(list_rep, SymbolicObject) or not is_instance_userdefined_and_newclass(list_rep):
+                return match_smt_type(to_pysmt(list_rep), unknown.get_type())
+            else:
+                print(list_rep, type(list_rep))
+                raise TypeError("Summaries don't support object returns")
+        else:
+            return unknown
+
+    def _to_list_rep(self, node):
+        if node is None:
+            return None
+        children = node.children
+        if len(children) == 2:
+            if node.children[0].predicate.symtype.symbolic_eq(node.children[1].predicate.symtype):
+                left = node.children[0] if node.children[0].predicate.result else node.children[1]
+                right = node.children[1] if not node.children[1].predicate.result else node.children[0]
+                return [pred_to_smt(left.predicate), self._to_list_rep(left), self._to_list_rep(right)]
+            else:
+                raise ValueError("Two children of a constraint should have the same predicate!")
+        elif len(node.children) == 1:
+            return [pred_to_smt(node.children[0].predicate), self._to_list_rep(node.children[0]), None]
+        elif len(children) == 0:
+            return node.effect
+
+        raise ValueError("Should not be possible! Can't have more than two children.")
